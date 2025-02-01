e30f41758afa561d69443169ea35ad1d6e38e2fc
Alexandre <alexandre.lourenco@outlook.com>
2025-02-01 00:35:19 -0300

    curso finalizado

--- python/crie-sua-primeira-aplicacao/app.py
+++ python/crie-sua-primeira-aplicacao/app.py
@@ -1,8 +1,13 @@
 import os
 
-restaurantes = ['Pizza', 'Hamburguer', 'Japonesa', 'Chinesa', 'Italiana']
+restaurantes = [{'nome':'Pizza', 'categoria':'Fast Food', 'ativo':True},
+                 {'nome':'Hamburguer', 'categoria':'Fast Food', 'ativo':True},
+                 {'nome':'Japonesa', 'categoria':'Fast Food', 'ativo':False},
+                 {'nome':'Chinesa', 'categoria':'Fast Food', 'ativo':True},
+                 {'nome':'Italiana', 'categoria':'Fast Food', 'ativo':True}]
 
 def exibir_nome_do_programa():
+    '''Essa função é utilizada para exibir o nome do programa'''
     print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
@@ -13,39 +18,83 @@ def exibir_nome_do_programa():
 """)
 
 def exibir_opcoes():
+    '''Essa função é utilizada para exibir as opções do menu principal'''
     print('1. Cadastrar Restaurante')
     print('2. Listar Restaurantes')
-    print('3. Ativar Restaurante')
+    print('3. Alternar Status Restaurante')
     print('4. Sair\n')
 
 def voltar_ao_menu_principal():
-    input('\nDigite uma tecla para continuar: ')
+    '''Essa função é utilizada para voltar ao menu principal'''
+    input('Digite uma tecla para continuar: ')
     main()
 
 def exibir_subtitulo(texto):
+    '''Essa função é utilizada para exibir um subtitulo'''
     os.system('clear')
+    linha = '-' * len(texto)
     print(texto)
-    print('--------------------------------------\n')
+    print(linha + '\n')
 
 def opcao_invalida():
+    '''Essa função é utilizada para exibir uma mensagem de opção inválida'''
     print('Opção inválida!\n')
     voltar_ao_menu_principal()
 
 def cadastrar_novo_restaurante():
+    '''Essa função é utilizada para cadastrar um novo restaurante
+    Inputs:
+    - Nome do restaurante
+    - Categoria
+
+    Outputs:
+    - Adiciona um novo restaurante na lista de restaurantes
+    '''
+
     exibir_subtitulo('Cadastro de novos restaurantes')
     nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
-    restaurantes.append(nome_do_restaurante)
-    print(f'Restaurante {nome_do_restaurante} cadastrado com sucesso!')
+    categoria_do_restaurante = input(f'Digite a categoria do restaurante {nome_do_restaurante}: ')
+    dados_do_restaurante = {'nome': nome_do_restaurante, 'categoria': categoria_do_restaurante, 'ativo': False}
+
+    restaurantes.append(dados_do_restaurante)
+    print(f'\nRestaurante {nome_do_restaurante} cadastrado com sucesso!\n')
+    voltar_ao_menu_principal()
+
+def alternar_estado_restaurante():
+    '''Essa função é utilizada para ativar ou desativar um restaurante'''
+    exibir_subtitulo('Alterando estado do restaurante')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja ativar/desativar: ')
+    restaurante_encontrado = False
+
+    for restaurante in restaurantes:
+        if restaurante['nome'] == nome_do_restaurante:
+            restaurante_encontrado = True
+            restaurante['ativo'] = not restaurante['ativo']
+            mensagem = f'\nRestaurante {nome_do_restaurante} ativado com sucesso!' if restaurante['ativo'] else f'\ndRestaurante {nome_do_restaurante} desativado com sucesso!'
+            print(mensagem + '\n')
+            break
+
+    if not restaurante_encontrado:
+        print(f'\nRestaurante {nome_do_restaurante} não encontrado!\n')
+
     voltar_ao_menu_principal()
 
 def listar_restaurantes():
+    '''Essa função é utilizada para listar todos os restaurantes cadastrados'''
     exibir_subtitulo('Listagem de restaurantes')
-    for restaurante in restaurantes:
-        print(f'.{restaurante}')
 
+    print('Nome do Restaurante'.ljust(22) + ' | ' + 'Categoria'.ljust(20) + ' | ' + 'Status')
+    for restaurante in restaurantes:
+        nome_restaurante = restaurante['nome']
+        categoria_restaurante = restaurante['categoria']
+        ativo = 'Sim' if restaurante['ativo'] else 'Não'
+        print(f'- {nome_restaurante.ljust(20)} | {categoria_restaurante.ljust(20)} | {ativo}')
+
+    print()
     voltar_ao_menu_principal()
 
 def escolher_opcoes():
+    '''Essa função é utilizada para escolher as opções do menu principal'''
     try:
         opcao_escolhida = int(input('Escolha uma opção: '))
         
@@ -54,7 +103,7 @@ def escolher_opcoes():
         elif opcao_escolhida == 2:
             listar_restaurantes()
         elif opcao_escolhida == 3:
-            print('Ativar Restaurante')
+            alternar_estado_restaurante()
         elif opcao_escolhida == 4:
             finalizar_aplicacao()
         else:
@@ -63,9 +112,11 @@ def escolher_opcoes():
         opcao_invalida()
 
 def finalizar_aplicacao():
+    '''Essa função é utilizada para finalizar a aplicação'''
     exibir_subtitulo('Obrigado por utilizar o Sabor Express!')
 
 def main():
+    '''Essa função é utilizada para executar o programa'''
     os.system('clear')
     exibir_nome_do_programa()
     exibir_opcoes()
